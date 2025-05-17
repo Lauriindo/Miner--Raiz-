d17c1e68c1d391e1e08db92020cb3f1284ebf874
Leandro <laurindo.ytbr@gmail.com>
2025-05-17 05:16:30 -0300

    a

--- data.py
+++ data.py
@@ -1,61 +1,9 @@
-import mysql.connector
+from sqlalchemy import create_engine #Estou startando a conexão com o banco de dados
+from sqlalchemy.orm import sessionmaker #Estou importando o sessionmaker para criar uma sessão com o banco de dados
+from sqlalchemy.ext.declarative import declarative_base #Estou importando o declarative_base para criar as classes que representam as tabelas do banco de dados
+db = create_engine("mysql+pymysql://root:1234@127.0.0.1:3306/Mineradora")#Aqui eu passo os parametros de conexão com o banco de dados, como Usuario:senha:ip do banco:nome
 
-def conectar():
-    return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="root",
-        database="Mineradora"
-    )
+Session = sessionmaker(bind=db) #Aqui eu crio uma sessão com o banco de dados
+session = Session() #Aqui eu crio uma sessão com o banco de dados
 
-def inserir(entidade, dados):
-    conn = conectar()
-    cursor = conn.cursor()
-
-    if entidade == "Maquinas":
-        cursor.execute("INSERT INTO maquinas (nome_maquina, capacidade_operacional, custo_aluguel_dia, custo_transporte) VALUES (%s, %s, %s, %s)", dados)
-    elif entidade == "Materiais":
-        cursor.execute("INSERT INTO materiais (nome_material, tipo, dureza) VALUES (%s, %s, %s)", dados)
-    elif entidade == "Clientes":
-        cursor.execute("INSERT INTO clientes (nome_cliente, cpf) VALUES (%s, %s)", dados)
-
-    conn.commit()
-    conn.close()
-
-def consultar(entidade):
-    conn = conectar()
-    cursor = conn.cursor(dictionary=True)
-
-    cursor.execute(f"SELECT * FROM {entidade.lower()}")
-    resultados = cursor.fetchall()
-
-    conn.close()
-    return resultados
-
-def excluir(entidade, id_item):
-    conn = conectar()
-    cursor = conn.cursor()
-
-    cursor.execute(f"DELETE FROM {entidade.lower()} WHERE id = %s", (id_item,))
-    conn.commit()
-    conn.close()
-
-def editar(entidade, id_item, novos_dados):
-    conn = conectar()
-    cursor = conn.cursor()
-
-    if entidade == "Maquinas":
-        cursor.execute("""
-            UPDATE maquinas SET nome_maquina=%s, capacidade_operacional=%s, custo_aluguel_dia=%s, custo_transporte=%s WHERE id=%s
-        """, (*novos_dados, id_item))
-    elif entidade == "Materiais":
-        cursor.execute("""
-            UPDATE materiais SET nome_material=%s, tipo=%s, dureza=%s WHERE id=%s
-        """, (*novos_dados, id_item))
-    elif entidade == "Clientes":
-        cursor.execute("""
-            UPDATE clientes SET nome_cliente=%s, cpf=%s WHERE id=%s
-        """, (*novos_dados, id_item))
-
-    conn.commit()
-    conn.close()
+declarative_base() #Aqui eu crio uma base declarativa para criar as classes que representam as tabelas do banco de dados
